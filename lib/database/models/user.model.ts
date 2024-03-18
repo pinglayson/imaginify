956aff907b1712452f60525cdc0247c3939b8630
Ping Layson <pinglayson@users.noreply.github.com>
2024-03-18 14:33:16 +1100

    fix(model): typo

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,26 +1,39 @@
-import { Document } from "mongodb";
 import { Schema, model, models } from "mongoose";
 
-// export interface IUser extends Document {
-//   clerkId: string;
-//   email: string;
-//   userName: string;
-//   photo?: URL;
-//   firstName?: string;
-//   lastName?: string;
-//   planId?: string;
-//   creditBalance?: number;
-// }
-
 const UserSchema = new Schema({
-  clerkId: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  userName: { type: String, required: true, unique: true },
-  photo: { type: String, required: true },
-  firstName: { type: String },
-  lastName: { type: String },
-  planId: { type: Number, default: 1 },
-  creditBalance: { type: Number, default: 10 },
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  planId: {
+    type: Number,
+    default: 1,
+  },
+  creditBalance: {
+    type: Number,
+    default: 10,
+  },
 });
 
 const User = models?.User || model("User", UserSchema);
